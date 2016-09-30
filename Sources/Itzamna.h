a026c1ae38cf4f99f8ebbc030f24433a8091ebbd
Phil J. Laszkowicz <phil@fillip.pro>
2016-09-30 23:29:45 +0300

    Cleaned project for code import.

---
+++ Sources/Itzamna.h
@@ -0,0 +1,12 @@
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Itzamna.
+FOUNDATION_EXPORT double ItzamnaVersionNumber;
+
+//! Project version string for Itzamna.
+FOUNDATION_EXPORT const unsigned char ItzamnaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Itzamna/PublicHeader.h>
+
+
